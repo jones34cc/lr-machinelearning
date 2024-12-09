3876cf38b6f3a017607ce697a759da5ae391145c
Mr. CHAMBER <godzoroastrian@gmail.com>
2024-12-09 23:31:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,10 @@ st.title('🐱‍🏍Diabetes Prediction App ')
 st.info('This app builds a machine learning model')
 
 with st.expander('Data'):
-  st.write('Raw data**')
+  st.write('**Raw data**')
   df=pd.read_csv('https://raw.githubusercontent.com/jones34cc/diabetes-data/refs/heads/main/diabetes_prediction_dataset.csv')
   df
+
+st.write('**X**')
+X=df.drop('diabetes',axis=1)
+X
