d5423167800073fc471ab71f4bcd9946dd9b730a
Mr. CHAMBER <godzoroastrian@gmail.com>
2024-12-10 01:28:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,7 +85,11 @@ clf.fit(X,Y)
 prediction=clf.predict(input_row)
 prediction_proba=clf.predict_proba(input_row)
 
-prediction_proba
+df_prediction_proba=pd.DataFrame(prediction_proba)
+df_prediction_proba.columns=['No Diabetes Motherfucker','Die Already Bruv']
+df_prediction_proba.rename(columns={0:'No Diabetes Motherfucker',
+                                 1:'Die Already Bruv'})
+df_prediction_proba
 
   
   
