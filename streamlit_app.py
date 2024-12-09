c8aef9bd7a57e1c8014c3d902a7de3e78f6d90bf
Mr. CHAMBER <godzoroastrian@gmail.com>
2024-12-10 00:21:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,5 +35,21 @@ with st.sidebar:
   bmi=st.slider("Adjust your BMI ",6,80)
   HbA1c_level=st.number_input('Enter your HbA1c blood level')
   blood_glucose_level=st.number_input('Enter your Blood Glucose level')
+
+  #create a dataframe for input features
+
+  data={'gender',gender,
+        'age',age,
+        'hypertension':hypertension,
+        'heart_disease':heart_disease,
+        'smoking_history':smoking_history,
+        'bmi':bmi,
+        'HbA1c_level':HbA1c_level
+        'blood_glucose_level':blood_glucose_level}
+  input_df=pd.DataFrame(data,index=[0])
+  input_features=pd.concat([input_df,X],axis=0)
+
+input_features
+  
   
   
